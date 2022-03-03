e6413c19a0c575bb3b4367910a7715d4a6f92796
GiollyJoestar <gioele826@gmail.com>
2022-03-03 16:34:58 +0100

    risolto bug che aggiornava nome del prodotto anche se non veniva inserito

--- modifica_prodotto.php
+++ modifica_prodotto.php
@@ -11,18 +11,17 @@ if(isset($_SESSION['permessi']))
         $conn = mysqli_connect($servername, $username, $password, $dbname);
         if (!$conn)
             die('Could not connect');
-        $id=$_POST['id_prodotto'];
-        if(isset($_POST['nome_prodotto']))
+        $id=$_POST['id_prodotto']; 
+        if(!empty($_POST['nome_prodotto']))
         {
             $nome=$_POST['nome_prodotto'];
             $sql="UPDATE prodotti SET prodotto='$nome' WHERE id=$id";
             if(mysqli_query($conn,$sql))
                 print "nome aggiornato<br>";
             else
-                print "errore ".mysqli_error($conn);
-            
+                print "errore ".mysqli_error($conn);          
         }
-        if(isset($_POST['prezzo']))
+        if(!empty($_POST['prezzo']))
         {
             $prezzo=$_POST['prezzo'];
             $sql="UPDATE prodotti SET prezzoxkg=$prezzo WHERE id=$id";
